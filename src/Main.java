aec2d16bea3aa2d9347fffb5e880612b087e3fe2
SeafoodFat1ger <15103461960@163.com>
2024-07-26 19:23:11 +0800

    e6

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        MyJvm myJvm = new MyJvm();
+        System.out.println("Start JVM Garbage Collection Simulation.");
         Scanner scanner = new Scanner(System.in);
-        Path path = new Path();
         while (scanner.hasNext()) {
             String operation = scanner.next();
-            if (operation.equals("CreatePath")) {
+            if (operation.equals("CreateObject")) {
                 int count = scanner.nextInt();
-                List<Integer> nodeList = new ArrayList<>();
-                for (int i = 1; i <= count; i++) {
-                    int node = scanner.nextInt();
-                    nodeList.add(node);
+                myJvm.createObject(count);
+                System.out.println("Create " + count + " Objects.");
+            } else if (operation.equals("SetUnreferenced")) {
+                List<Integer> unrefList = new ArrayList<>();
+                while (scanner.hasNextInt()) {
+                    int id = scanner.nextInt();
+                    unrefList.add(id);
+                    System.out.println("Set id: " + id + " Unreferenced Object.");
                 }
-                path = new Path(nodeList.stream().mapToInt(i -> i).toArray());
-                System.out.println("Create " + path);
-            } else if (operation.equals("Add")) {
-                int x = scanner.nextInt();
-                int u = scanner.nextInt();
-                int v = scanner.nextInt();
-                try {
-                    path.addPoint(x, u, v);
-                    System.out.println("Add Success");
-                } catch (Exception e) {
-                    if (e instanceof EdgeNotFoundException) {
-                        System.out.println("EdgeNotFoundException");
-                    } else if (e instanceof PathIsNotValid) {
-                        System.out.println("PathIsNotValidException");
-                    } else {
-                        System.out.println("System Error");
-                    }
-                }
-            } else if (operation.equals("ExtractLoopPath")) {
-                Path ans = path.extractLoopPath();
-                if (ans == null) {
-                    System.out.println("null");
-                } else {
-                    System.out.println(ans);
-                }
-            } else if (operation.equals("PrintPath")) {
-                System.out.println(path);
-            } else if (operation.equals("End")) {
-                break;
+                myJvm.setUnreferenced(unrefList);
+            } else if (operation.equals("SnapShot")) {
+                myJvm.getSnapShot();
+            } else {
+                System.err.println("Invalid operation.");
             }
         }
+        scanner.close();
+        System.out.println("End of JVM Garbage Collection Simulation.");
     }
 }
--- src/PathIsNotValid.java
+++
@@ -1,3 +0,0 @@
-class PathIsNotValid extends Exception {
-
-}
